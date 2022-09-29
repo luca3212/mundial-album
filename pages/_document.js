e4b04e568df3e9ce2c566a498bb71cebffb51aca
luca <luca-93@live.com.ar>
2022-09-29 09:29:01 -0300

    head

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="2wp3Io3jaz4d9zmtwKcSEqYyMNx8rAl--m8vtYFdW3s"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
